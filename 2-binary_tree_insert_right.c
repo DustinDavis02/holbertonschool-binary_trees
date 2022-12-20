41a06feec9e252f471dcdbca7632eda1e0ab7559
Dustin Davis <davisdustin328@gmail.com>
2022-12-19 21:31:21 -0600

    Delete a binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,11 +10,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent != NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
-	if (new != NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->parent = parent;
